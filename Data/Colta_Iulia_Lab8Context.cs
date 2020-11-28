1024ba8023bba3c749bc4991ee6050239646e3c6
IULIA MARIA COLTA <colta.iulia165249@student.ubbcluj.ro>
2020-11-28 11:37:35 +0200

    Add project files.

---
+++ Data/Colta_Iulia_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Colta_Iulia_Lab8.Models;
+
+namespace Colta_Iulia_Lab8.Data
+{
+    public class Colta_Iulia_Lab8Context : DbContext
+    {
+        public Colta_Iulia_Lab8Context (DbContextOptions<Colta_Iulia_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Colta_Iulia_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Colta_Iulia_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Colta_Iulia_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Colta_Iulia_Lab8.Data;
+using Colta_Iulia_Lab8.Models;
+
+namespace Colta_Iulia_Lab8.Pages.Publishers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Colta_Iulia_Lab8.Data.Colta_Iulia_Lab8Context _context;
+
+        public DeleteModel(Colta_Iulia_Lab8.Data.Colta_Iulia_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Publisher = await _context.Publisher.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Publisher == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Publisher = await _context.Publisher.FindAsync(id);
+
+            if (Publisher != null)
+            {
+                _context.Publisher.Remove(Publisher);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
